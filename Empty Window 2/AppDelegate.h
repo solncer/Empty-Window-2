7cc375863d7ccf452d61ca5853cb54956f12b393
Admin <krutova@yandex.ru>
2015-05-18 12:00:01 +0300

    Initial Commit

---
+++ Empty Window 2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Empty Window 2
+//
+//  Created by Admin on 18.05.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Empty Window 2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Empty Window 2
+//
+//  Created by Admin on 18.05.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
